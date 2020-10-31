8c03a51fc1034a51ce71c94451fdc90a7aec7875
eevaliisa <eevalr@gmail.com>
2020-10-31 19:27:30 +0200

    added signup page contents and styling

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = requre("request");
+const request = require("request");
+const https = require("https");
 
 const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "Subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+
+
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
